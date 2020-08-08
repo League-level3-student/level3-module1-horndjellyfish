1b130bb8e3302e98d259df4b86bf48c361d05a91
horndjellyfish <hornedjellyfish@gmail.com>
2020-08-07 17:01:11 -0700

    Long Chip Competition - unfinished
    day 8

--- src/_01_IntroToArrayLists/_05_LongChipCompetition.java
+++ src/_01_IntroToArrayLists/_05_LongChipCompetition.java
@@ -6,69 +6,77 @@ import java.util.Random;
 import javax.swing.JOptionPane;
 
 public class _05_LongChipCompetition {
-	/**
-	 * The Beatles are eating lunch and playing a game to see who has the longest
-	 * chip. (In England, french fries are called "chips".) * Find the Beatle with
-	 * the longest chip. You may not edit the Chip or Beatle classes. Make sure to
-	 * initialize The Beatles before you start your search. *
-	 **/
-	private ArrayList<Beatle> theBeatles = new ArrayList<Beatle>();
+    /**
+     * The Beatles are eating lunch and playing a game to see who has the longest
+     * chip. (In England, french fries are called "chips".) * Find the Beatle with
+     * the longest chip. You may not edit the Chip or Beatle classes. Make sure to
+     * initialize The Beatles before you start your search. *
+     **/
+    private ArrayList<Beatle> theBeatles = new ArrayList<Beatle>();
 
-	public static void main(String[] args) {
-		_05_LongChipCompetition lcc = new _05_LongChipCompetition();
-		lcc.initializeBeatles();
-		lcc.getTheBand();
-	}
-	
-	private void initializeBeatles() {
-		Beatle george = new Beatle("George");
-		Beatle john = new Beatle("John");
-		Beatle paul = new Beatle("Paul");
-		Beatle ringo = new Beatle("Ringo");
-		theBeatles.add(george);
-		theBeatles.add(john);
-		theBeatles.add(paul);
-		theBeatles.add(ringo);
-	}
-	
-	public ArrayList<Beatle> getTheBand(){
-		return theBeatles;
-	}
+    public static void main(String[] args) {
+        _05_LongChipCompetition lcc = new _05_LongChipCompetition();
+        lcc.initializeBeatles();
+        lcc.getTheBand();
+    }
+
+    private void initializeBeatles() {
+        Beatle george = new Beatle("George");
+        Beatle john = new Beatle("John");
+        Beatle paul = new Beatle("Paul");
+        Beatle ringo = new Beatle("Ringo");
+        theBeatles.add(george);
+        theBeatles.add(john);
+        theBeatles.add(paul);
+        theBeatles.add(ringo);
+    }
+
+    public ArrayList<Beatle> getTheBand() {
+        for (int i = 0; i < theBeatles.size(); i++) {
+            Beatle beatle = theBeatles.get(i);
+            ArrayList<Chip> chip = beatle.getChips();
+            for (i = 0; i < chip.size(); i++) {
+                double length = chip.get(i).getLength();
+                
+            }
+        }
+        return theBeatles;
+    }
 }
 
 class Beatle {
-	private String name;
-	private ArrayList<Chip> chips = new ArrayList<Chip>();
+    private String name;
+    private ArrayList<Chip> chips = new ArrayList<Chip>();
 
-	public Beatle(String name) {
-		this.name = name;
-		initializePlateOfChips();
-	}
+    public Beatle(String name) {
+        this.name = name;
+        initializePlateOfChips();
+    }
 
-	private void initializePlateOfChips() {
-		int numberOfChips = new Random().nextInt(100);
-		for (int i = 0; i < numberOfChips; i++) {
-			chips.add(new Chip(new Random().nextDouble() * 10));
-		}
-	}
+    private void initializePlateOfChips() {
+        int numberOfChips = new Random().nextInt(100);
+        for (int i = 0; i < numberOfChips; i++) {
+            chips.add(new Chip(new Random().nextDouble() * 10));
+        }
+    }
 
-	public ArrayList<Chip> getChips() {
-		return this.chips;
-	}
+    public ArrayList<Chip> getChips() {
+        return this.chips;
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    public String getName() {
+        return this.name;
+    }
 }
 
 class Chip {
-	private double length;
+    private double length;
 
-	public double getLength() {
-		return length;
-	}
+    public double getLength() {
+        return length;
+    }
 
-	Chip(double d) {
-		this.length = d;
-	}
+    Chip(double d) {
+        this.length = d;
+    }
 }
