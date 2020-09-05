f3ff154754973d0dca4750aab00036a49b30ee72
horndjellyfish <hornedjellyfish@gmail.com>
2020-09-05 11:25:59 -0700

    IntroToStack - complete, TextUndoRedo - incomplete
    day 13

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -1,38 +1,53 @@
 package _03_IntroToStacks;
 
 import javax.swing.*;
+import java.util.Random;
 import java.util.Stack;
 
 public class _01_IntroToStack {
-	public static void main(String[] args) {
-		//1. Create a Stack of Doubles
-		//   Don't forget to import the Stack class
-		Stack<Double> doubles = new Stack<Double>();
-		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		for (double i = 0; i <= 100; i++) {
-			doubles.push(Math.floor(Math.random()* 100) + 1);
-		}
+    public static void main(String[] args) {
+        //1. Create a Stack of Doubles
+        //   Don't forget to import the Stack class
+        Stack<Double> doubles = new Stack<Double>();
+        //2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+        Random rand = new Random(100);
+        for (double i = 0; i <= 100; i++) {
+            double random = rand.nextDouble() * 100;
+           doubles.push(random);
 
-		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		String input = JOptionPane.showInputDialog("Enter two numbers between 0 and 100");
-		//4. Pop all the elements off of the Stack. Every time a double is popped that is
-		//   between the two numbers entered by the user, print it to the screen.
-		System.out.println("Popping elements off stack...");
-		System.out.println(doubles.pop());
-		System.out.println("Elements between " + input);
-		//double d = Double.parseDouble(input);
-		//   EXAMPLE:
-		//   NUM 1: 65
-		//   NUM 2: 75
-		
-		//   Popping elements off stack...
-		//   Elements between 65 and 75:
-		//   66.66876846
-		//   74.51651681
-		//   70.05110654
-		//   69.21350456
-		//   71.54506465
-		//   66.47984807
-		//   74.12121224
-	}
+        }
+
+        //3. Ask the user to enter in two numbers between 0 and 100, inclusive.
+        String input = JOptionPane.showInputDialog("Enter a number between 0 and 100");
+        String input1 = JOptionPane.showInputDialog("Enter a number between 0 and 100 that is larger than the number you previously entered");
+        //4. Pop all the elements off of the Stack. Every time a double is popped that is
+        //   between the two numbers entered by the user, print it to the screen.
+        System.out.println("Popping all the elements off the stack...");
+        int sz = doubles.size();
+        double minDouble = Double.parseDouble(input);
+        double maxDouble = Double.parseDouble(input1);
+        System.out.println("Elements between " + minDouble + " and " + maxDouble + ":");
+        for (int i = 0; i < sz; i++) {
+            Double popped = doubles.pop();
+
+            if ((popped >= minDouble) && (popped <= maxDouble)) {
+                System.out.println(popped.doubleValue());
+            }
+        }
+
+
+        //   EXAMPLE:
+        //   NUM 1: 65
+        //   NUM 2: 75
+
+        //   Popping elements off stack...
+        //   Elements between 65 and 75:
+        //   66.66876846
+        //   74.51651681
+        //   70.05110654
+        //   69.21350456
+        //   71.54506465
+        //   66.47984807
+        //   74.12121224
+    }
 }
--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,6 +1,11 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class _02_TextUndoRedo implements KeyListener {
+
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,6 +18,39 @@ public class _02_TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
-	
-	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+
+	public static void main (String [] args) {
+		_02_TextUndoRedo tur = new _02_TextUndoRedo();
+		tur.createFrame();
+	}
+
+	public void createFrame() {
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		label.addKeyListener(this);
+		//label.add();
+		panel.add(label);
+		frame.add(panel);
+		frame.pack();
+	}
+
+
+	public void keyTyped(KeyEvent e) {
+		if (e.getSource().equals(char.class)) {
+			
+		}
+	}
+
+
+	public void keyPressed(KeyEvent e) {
+
+	}
+
+
+	public void keyReleased(KeyEvent e) {
+
+	}
 }
