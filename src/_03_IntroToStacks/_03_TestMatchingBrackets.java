a59c12ffeda3b8e9eb6091244fdbdf076456abe8
horndjellyfish <hornedjellyfish@gmail.com>
2020-09-26 11:31:38 -0700

    TestMatchingBrackets - complete, HangMan - incomplete
    day 16

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -1,50 +1,67 @@
 package _03_IntroToStacks;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 import java.util.Stack;
 
-import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class _03_TestMatchingBrackets {
 
-	@Test
-	public void testMatchingBrackets() {
-		assertTrue(doBracketsMatch("{}"));
-		assertTrue(doBracketsMatch("{{}}"));
-		assertTrue(doBracketsMatch("{}{}{{}}"));
-		assertFalse(doBracketsMatch("{{}"));
-		assertFalse(doBracketsMatch("}{"));
-	}
+    @Test
+    public void testMatchingBrackets() {
+        assertTrue(doBracketsMatch("{}"));
+        assertTrue(doBracketsMatch("{{}}"));
+        assertTrue(doBracketsMatch("{}{}{{}}"));
+        assertFalse(doBracketsMatch("{{}"));
+        assertFalse(doBracketsMatch("}{"));
+    }
 
-	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
-	private boolean doBracketsMatch(String brackets) {
-		
-		//1. Use a for loop to iterate through your brackets String 
+    // USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
+    private boolean doBracketsMatch(String brackets) {
+        Stack<Character> stack = new Stack<Character>();
+        //1. Use a for loop to iterate through your brackets String
+        char[] chars = brackets.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char current = chars[i];
+            if (current == '{') {
+                stack.push(current);
+            } else if (current == '}') {
+                if (stack.size() == 0) {
+                    return false;
+                }
+                stack.pop();
+            }
 
-    			//2.  If the current character is an '{'
+        }
+        if (stack.size() > 0) {
+            return false;
+        } else {
+            return true;
+        }
+        //2.  If the current character is an '{'
 
-        			//3.  Push an '{' onto the stack 
+        //3.  Push an '{' onto the stack
 
-    			//4.  else if the character is a '}'
+        //4.  else if the character is a '}'
 
-        			//6.  if the stack is empty (i.e. there is no matching opening bracket)
+        //6.  if the stack is empty (i.e. there is no matching opening bracket)
 
-            				//7.  return false 
+        //7.  return false
 
-        			//5.  Pop a character from the stack
+        //5.  Pop a character from the stack
 
 
-		//8.  if the stack is not empty (i.e. there were too many opening brackets)
+        //8.  if the stack is not empty (i.e. there were too many opening brackets)
 
-    			//9. return false 
+        //9. return false
 
-		//10. else (i.e. everything matched correctly)
+        //10. else (i.e. everything matched correctly)
 
-    			//11. return true 
-			return true;
-		
-	}
+        //11. return true
+
+
+    }
 
 }
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,32 @@
 package _04_HangMan;
 
-public class HangMan{
-	
-	
-	
+import javax.swing.*;
+import java.util.Stack;
+
+public class HangMan {
+    Stack<String> lines = new Stack<String>();
+
+    public static void main(String[] args) {
+        HangMan hm = new HangMan();
+        hm.getWords();
+    }
+
+    public void getWords() {
+        String input = JOptionPane.showInputDialog("Enter a number.");
+        int words = Integer.parseInt(input);
+        for (int i = 0; i < words; i++) {
+            String word = Utilities.readRandomLineFromFile("dictionary.txt");
+            lines.push(word);
+        }
+
+    }
+
+    public void createGame() {
+        String word = lines.pop();
+        JFrame frame = new JFrame();
+        JLabel label = new JLabel(word);
+        frame.add(label);
+        frame.pack();
+
+    }
 }
