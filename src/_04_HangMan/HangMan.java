725666952082c8b0e2a378ccfdc1e865df14582b
horndjellyfish <hornedjellyfish@gmail.com>
2020-10-17 11:32:31 -0700

    Hangman - incomplete (almost finished i swear)
    day 19

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -4,13 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class HangMan implements KeyListener {
     Stack<String> lines = new Stack<String>();
     JLabel label = new JLabel();
     JFrame frame;
-    char[] letters;
+    char[] hangmanDisplayWord;
+    char[] hiddenWord;
+    char input;
+
     public static void main(String[] args) {
         HangMan hm = new HangMan();
         hm.getWords();
@@ -31,20 +35,16 @@ public class HangMan implements KeyListener {
 
     public void createGame() {
         String word = lines.pop();
-        letters = word.toCharArray();
+        hangmanDisplayWord = word.toCharArray();
+        hiddenWord = word.toCharArray();
         String display = "";
         System.out.println(word);
         char lines = '_';
-        String inp = JOptionPane.showInputDialog("Guess a letter.");
-        char input = inp.charAt(0);
-        for (int i = 0; i < letters.length; i++) {
-            letters[i] = lines;
-            display = display + letters[i] + " ";
-            if (letters[i] == input) {
-                
-            }
+        for (int i = 0; i < hangmanDisplayWord.length; i++) {
+            hangmanDisplayWord[i] = lines;
+            display = display + hangmanDisplayWord[i] + " ";
         }
-        Font font = new Font("Courier", Font.BOLD,12);
+        Font font = new Font("Courier", Font.BOLD, 12);
         label.setFont(font);
         label.setText(display);
         frame = new JFrame();
@@ -52,13 +52,52 @@ public class HangMan implements KeyListener {
         frame.setSize(300, 300);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.addKeyListener(this);
-
         frame.add(label);
         //frame.pack();
-
+        guessLetter();
     }
+
+    public void setText(char[] word) {
+        String display = "";
+        for (int i = 0; i < word.length; i++) {
+            display = display + word[i] + " ";
+        }
+        label.setText(display);
+    }
+
+    public void guessLetter() {
+        String inp = JOptionPane.showInputDialog("Guess a letter.");
+        input = inp.charAt(0);
+        ArrayList<Integer> newMatches = findMatches(input);
+        if (newMatches.size() > 0) {
+            populateLetters(newMatches, input);
+            setText(hangmanDisplayWord);
+        } else {
+            // take off a life
+        }
+    }
+
+    public void populateLetters(ArrayList<Integer> matches, char letter) {
+       for (int i = 0; i < matches.size(); i++) {
+            int small = matches.get(i).intValue();
+            hangmanDisplayWord[small] = letter;
+       }
+    }
+
+    public ArrayList<Integer> findMatches(char letter) {
+        ArrayList<Integer> matches = new ArrayList<>();
+        for (int i = 0; i < hiddenWord.length; i++) {
+            if (hiddenWord[i] == letter) {
+                matches.add(i);
+            }
+        }
+
+        return matches;
+    }
+
     public void keyPressed(KeyEvent e) {
-        char input = e.getKeyChar();
+        //input = e.getKeyCode();
+
     }
 
 
